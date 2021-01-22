4f0c1e22d3e95f94ad81da7640d5889ad12e14b2
ketoprom <32978578+ketoprom@users.noreply.github.com>
2021-01-22 16:10:25 +0100

    Added Utilities Class
    Added Utilities Class with methods for:
    Checking if file is Zip archive
    Unpack Zip archive to desired path
    Clean Directory if folder exist in given directory
    Get List of all files directories in given directory

---
+++ CheckForPhotoInDirectory/Utilities.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace CheckForPhotoInDirectory
+{
+    public static class Utilities
+    {
+        public static Boolean checkIfFileIsZipArchive(String filePath)
+        {
+            try
+            {
+                using (var zipFile = ZipFile.OpenRead(filePath))
+                {
+                    var entries = zipFile.Entries;
+                    return true;
+                }
+            }
+            catch (InvalidDataException)
+            {
+                return false;
+            }
+        }
+        public static void unPack(String archivePatch, string extractPath)
+        {
+            cleanDirectoryIfFolderExists(extractPath);
+            ZipFile.ExtractToDirectory(archivePatch, extractPath);
+        }
+        public static void cleanDirectoryIfFolderExists(string extractPath)
+        {
+            if (Directory.Exists(extractPath))
+            {
+                Directory.Delete(extractPath, recursive: true);
+            }
+        }
+        public static string[] getListOfAllFilesDirectories(string directoryPath)
+        {
+            string[] files = Directory.GetFiles(directoryPath, "*", SearchOption.AllDirectories);
+            return files;
+        }
+
+    }
+}
