3ebe14d2f12ef9e15d0b290532762b11e00d9066
ketoprom <32978578+ketoprom@users.noreply.github.com>
2021-01-22 17:33:45 +0100

    Create FileToBeCheckedForPhoto.cs
    Created class that holds  file directory, file name and information if file contains/is photo. Added methot that cheks whether the file is a doc and contains image inside.

---
+++ CheckForPhotoInDirectory/FileToBeCheckedForPhoto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Spire.Doc.Documents;
+using Spire.Doc;
+
+
+namespace CheckForPhotoInDirectory
+{
+    public class FileToBeCheckedForPhoto
+    {
+        public String FileDirectory { get;}
+        public String FileName { get; }
+        public Boolean ContainsPhoto { get; private set; }
+
+        public FileToBeCheckedForPhoto(String filePath)
+        {
+            FileDirectory = filePath;
+            FileName = Path.GetFileName(filePath);
+            ContainsPhoto = false;
+        }
+        //Method to check if doc file contains Image using Spire.Doc
+        public Boolean checkForImageInDocFile()
+        {
+            try
+            {
+                Document document = new Document(this.FileDirectory);
+                {
+                    foreach (Section section in document.Sections)
+                    {
+                        //Get Each Paragraph of Section
+                        foreach (Paragraph paragraph in section.Paragraphs)
+                        {
+                            //Get Each Document Object of Paragraph Items
+                            foreach (DocumentObject docObject in paragraph.ChildObjects)
+                            {
+                                //If Type of Document Object is Picture set ContainsPhoto property to true and return true.
+                                if (docObject.DocumentObjectType == DocumentObjectType.Picture)
+                                {
+                                    this.ContainsPhoto = true;
+                                    return true;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception) { }
+            return false;
+        }
+
+    }
+}
