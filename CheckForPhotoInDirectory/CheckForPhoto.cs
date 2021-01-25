6d3ed3eb49ac08986c77601bce9c13884b35cbb6
ketoprom <32978578+ketoprom@users.noreply.github.com>
2021-01-25 09:39:06 +0100

    Update CheckForPhoto.cs
    Main program

--- CheckForPhotoInDirectory/CheckForPhoto.cs
+++ CheckForPhotoInDirectory/CheckForPhoto.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace CheckForPhotoInDirectory
 {
@@ -6,7 +7,46 @@ namespace CheckForPhotoInDirectory
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Initial Commit");
+            //Path to extract zip file
+            string extractPath = @".\extractPath"; 
+
+            try
+            { 
+                List<FileToBeCheckedForPhoto> listOfFileObjects = new List<FileToBeCheckedForPhoto>();
+
+                if (args.Length == 0)
+                {
+                    Console.WriteLine("Podaj ścieżkę do pliku");
+                }
+                // If the given directory is Zip archive, unpack it and create list of objects of all files that it contains
+                else if (Utilities.CheckIfFileIsZipArchive(args[0]))
+                {
+                    Utilities.UnPackZipArchive(args[0], extractPath);
+                    String[] listOfFilesDirectories = Utilities.GetListOfAllFilesInFolderAndSubfolder(extractPath);
+                    listOfFileObjects = Utilities.CreateListOfFileObjects(listOfFilesDirectories);
+                }
+                // If it's not zip archive then it's signle file that needs to be checked
+                else
+                {
+                    listOfFileObjects.Add(new FileToBeCheckedForPhoto(args[0]));
+                }
+                // All files are being checked for being jpg or png image or did they contain jpg or png image
+                foreach (var item in listOfFileObjects)
+                {
+                    if (item.CheckForImageInDocFile() || item.CheckIfFileIsImage() || 
+                        item.CheckForImageInPowerPointFile() || item.CheckForImageInSpreadsheetFile()){}
+                    Console.WriteLine(item);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            finally
+            {
+                // Delete prevoriusly created folder
+                Utilities.CleanDirectoryIfFolderExists(extractPath);
+            }
         }
     }
 }
